6737d97ba9f83414a02c010eeef98dea7431a63f
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-26 18:25:27 +0000

    fixed capitalization error

--- lib/MoviesCli/api.rb
+++ lib/MoviesCli/api.rb
@@ -2,7 +2,7 @@ class MoviesCli::API
   def fetch 
     key = "d55db91a2c26ae6c705d3ec14e5e2a0e"
     url = "https://api.themoviedb.org/3/movie/550?api_key=d55db91a2c26ae6c705d3ec14e5e2a0e"
-    response = HTTparty.get(url)
+    response = HTTParty.get(url)
     binding.pry
   end 
 end 
