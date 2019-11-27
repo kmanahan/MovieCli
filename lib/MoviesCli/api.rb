34587b5e60eb6b824898db7065bba7591237437b
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-27 22:08:45 +0000

    api call update

--- lib/MoviesCli/api.rb
+++ lib/MoviesCli/api.rb
@@ -3,10 +3,13 @@ class MoviesCli::API
     key = "d55db91a2c26ae6c705d3ec14e5e2a0e"
     url = "https://api.themoviedb.org/3/discover/movie?api_key=d55db91a2c26ae6c705d3ec14e5e2a0e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
     response = HTTParty.get(url)
-    binding.pry
-    title = response["title"]
-    release_date = response["release_date"]
-    overview = response["overview"]
-    MoviesCli::Movies.new(title, release_date)
+    #binding.pry
+    response["results"].each do |movie| 
+      title = movie["title"]
+      popularity = movie["popularity"]
+      # overview = movie["overview"]
+      # release_date = movie["release_date"]
+      MoviesCli::Movies.new(popularity, title)
+    end
   end 
 end 
