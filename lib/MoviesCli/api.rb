04248e2330b783fa66d0b9efcdb6ffa77e372fe6
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-12-03 18:52:42 +0000

    removed comment

--- lib/MoviesCli/api.rb
+++ lib/MoviesCli/api.rb
@@ -3,7 +3,6 @@ class API
     key = "d55db91a2c26ae6c705d3ec14e5e2a0e"
     url = "https://api.themoviedb.org/3/discover/movie?api_key=d55db91a2c26ae6c705d3ec14e5e2a0e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
     response = HTTParty.get(url)
-    #binding.pry
     response["results"].each do |movie| 
       title = movie["title"]
       overview = movie["overview"]
